e152ab6d81876157938f4bb9346ce48934eee705
Sasha <shlaw@bk.ru>
2025-03-25 18:34:37 +0300

    Initial Commit

---
+++ HW 2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HW 2
+//
+//  Created by Alexander Shprits on 25.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
