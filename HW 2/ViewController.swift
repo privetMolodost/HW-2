c3fbd14ad1f9c7ca8faaacd9d2946ae58f4e7eb0
Sasha <shlaw@bk.ru>
2025-03-25 22:50:12 +0300

    made logic and view of traffic light

--- HW 2/ViewController.swift
+++ HW 2/ViewController.swift
@@ -8,15 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
+    @IBOutlet var actionButton: UIButton!
+    
+    enum TrafficLightStatus {
+        case turnedOff
+        case turnedRed
+        case turnedYellow
+        case turnedGreen
+    }
+    
+    private var switchedOff = 0.3
+    private var switchedOn = 1.0
+    private var cornerRadius = 50.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        actionButton.layer.cornerRadius = 10
+        redLight.layer.cornerRadius = cornerRadius
+        redLight.alpha = switchedOff
+        
+        yellowLight.layer.cornerRadius = cornerRadius
+        yellowLight.alpha = switchedOff
+        
+        greenLight.layer.cornerRadius = cornerRadius
+        greenLight.alpha = switchedOff
     }
-
-
-}
+    
+    var currentStatus: TrafficLightStatus = .turnedGreen
+    
+    @IBAction func trafficButtonDidTapped(_ sender: UIButton) {
+        
+            switch currentStatus {
+            case .turnedOff:
+                greenLight.alpha = switchedOff
+                currentStatus = .turnedRed
+                sender.setTitle("Start", for: .normal)
+            case .turnedRed:
+                redLight.alpha = switchedOn
+                currentStatus = .turnedYellow
+                sender.setTitle("Next", for: .normal)
+            case .turnedYellow:
+                redLight.alpha = switchedOff
+                yellowLight.alpha = switchedOn
+                currentStatus = .turnedGreen
+                sender.setTitle("Next", for: .normal)
+            case .turnedGreen:
+                yellowLight.alpha = switchedOff
+                greenLight.alpha = switchedOn
+                currentStatus = .turnedOff
+                sender.setTitle("Stop", for: .normal)
+            }
+        }
+    }
 
